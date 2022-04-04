06a0a83cc9482b45a732a04c2400ca6b7a74bbf8
Kashyap Narayanan <64733066+kashyapnarayanan@users.noreply.github.com>
2022-04-04 12:26:28 +0530

    Added Index.html,style.css & script.js files

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let secretNumber = Math.floor(Math.random() * 20 + 1);
+let score = 20;
+let highScore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  //when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '😕 No Number';
+  }
+  //when enter invalid number
+  else if (guess < 0 || guess > 20) {
+    document.querySelector('body').style.backgroundColor = '#cc3300';
+    document.querySelector('.message').textContent =
+      '❌ Enter a Value Between 1 and 20';
+  }
+  //when enter valid number
+  else if (guess <= 20) {
+    if (guess === secretNumber) {
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.message').textContent = '😍 Correct Number';
+      document.querySelector('.score').textContent = score;
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
+
+      //When user enter low value
+    } else if (guess < secretNumber) {
+      if (score > 0) {
+        document.querySelector('body').style.backgroundColor = '#b38600';
+        document.querySelector('.message').textContent = '📉 Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('body').style.backgroundColor = '#cc3300';
+        document.querySelector('.message').textContent =
+          'You exceeded the maximum attempts';
+      }
+
+      //When user enter high value
+    } else if (guess > secretNumber) {
+      if (score > 0) {
+        document.querySelector('body').style.backgroundColor = '#800060';
+        document.querySelector('.message').textContent = '📈 Too High';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('body').style.backgroundColor = '#cc3300';
+        document.querySelector('.message').textContent =
+          'You exceeded the maximum attempts';
+      }
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.floor(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
